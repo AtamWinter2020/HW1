193967ee7c0200a24b098ff7fd0dd2b2a8c86b61
Gur Telem <gur111telem@gmail.com>
2020-11-06 09:12:47 -0800

    Ex1 passing

---
+++ ex1.asm
@@ -0,0 +1,19 @@
+.global _start
+
+.section .text
+_start:  
+  movq %rsp, %rbp #for correct debugging
+  movq num, %rdx
+  movl %edx, %ecx
+  andl $1, %ecx
+loop:
+  cmpq $0, %rdx
+  jz end
+  shrq $1, %rdx
+  test $1, %rdx
+  jz loop
+  addl $1, %ecx
+  jmp loop
+  
+end:
+  movq %rcx, (countBits)
