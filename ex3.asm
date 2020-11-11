108a7f5843349a85038fa02a68e357d28df6151b
Eliran Turgeman <50831652+DevEliran@users.noreply.github.com>
2020-11-11 17:46:10 +0000

    ex3 passing

--- ex3.asm
+++ ex3.asm
@@ -2,4 +2,20 @@
 
 .section .text
 _start:
-  #Write your code here
+    xor %rax, %rax #sum
+    xor %rcx, %rcx #index
+    xor %rsp, %rsp #temporary value for current array element
+    movq $arr, %rdi #adress of array in rdi
+    movw len, %bx #len
+loop:
+    movl (%rdi, %rbx, 4), %esp
+    addq %rsp, %rax
+    dec %bx
+    cmp $0, %bx
+    jne loop
+end:
+    movw len, %bx
+    xor %rdx, %rdx
+    div %rbx
+    movq %rax, avg
+    
