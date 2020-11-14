da6ab4b2b14dd19495e6745bd2e0a15af684586b
Eliran Turgeman <50831652+DevEliran@users.noreply.github.com>
2020-11-14 10:47:26 +0000

    ex4 passing

--- ex4.asm
+++ ex4.asm
@@ -2,4 +2,30 @@
 
 .section .text
 _start:
-  #Write your code here
+    movq $root, %rax
+    cmp $0, %rax
+    je empty_tree
+    movq $new_node, %rbx
+traverse:
+    cmpq %rax, %rbx
+    je end #if root ==  new_node
+    movq %rax, %rcx #backup
+    jl left
+    jmp right
+left:
+    movq 8(%rax), %rax
+    cmp $0, %rax #reached a leaf
+    jne traverse
+    movq $new_node, 8(%rcx)
+    jmp end
+right:
+    movq 16(%rax), %rax
+    cmp $0, %rax #reached a leaf
+    jne traverse
+    movq $new_node, 16(%rcx)
+    jmp end
+empty_tree:
+    movq $new_node, root
+    jmp end
+    
+end:
