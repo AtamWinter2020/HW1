84e5487220e4c749d35f591f42c3b8ccfeb7bbb8
Gur Telem <gtelem@apple.com>
2020-11-18 02:15:05 +0200

    Revert empty tree fix. Oops

--- ex4.asm
+++ ex4.asm
@@ -16,22 +16,17 @@ left:
     movq 8(%rax), %rax # Update current to get left leaf address
     cmp $0, %rax # Check if left leaf is null
     jne traverse # If not null compare again
-    movq $new_node, 8(%rcx) # Save new_node as left leaf
+    movq %rbx, 8(%rcx) # Save new_node as left leaf
     jmp end
 right:
     movq 16(%rax), %rax  # Update current to get right leaf address
     cmp $0, %rax # Check if right leaf is null
     jne traverse  # If not null compare new current
-    movq $new_node, 16(%rcx)  # Save new_node as right leaf
+    movq %rbx, 16(%rcx)  # Save new_node as right leaf
     jmp end
 empty_tree:
-    # Update root to be the new leaf
-    movq (new_node), %eax
-    movq %eax, (root)
-    movq 8(new_node), %eax
-    movq %eax, 8(root)
-    movq 16(new_node), %eax
-    movq %eax, 16(root)
+    # Update root (%rcx) to be the new leaf (%rbx). Temp values saved to %rax
+    movq $new_node, root
     jmp end
     
 end:
